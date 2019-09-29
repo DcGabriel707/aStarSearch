4acec5f3297a727c0d7cf575fb00cd7b9fd52505
gddelcar <dcgabriel707>
2019-09-29 01:10:42 -0500

    better structure. todo fix heuristic value inputs.

--- src/Main.java
+++ src/Main.java
@@ -4,114 +4,199 @@ import java.util.*;
 
 public class Main {
 
-    static Queue<Long> openList;
-    static ArrayList<Long> closedList;
+    static Queue<Node> openList; //open nodes queue
+    static ArrayList<Node> closedList;
     static Map<Long, Double> g; //cost of getting from the initial node to n
     static Map<Long, Double> h; //the estimate, eccording to the heuristic function, of the cost of getting n to the goal node
     static Map<Long, Double> f; // g(n) + h(n) estimate of the best solution that foes through n
     static long startingNode = 105050228; //change later
     static long goalNode = 106108549;
-    static ArrayList<Node> allNodes;
+    //static ArrayList<Node> allNodes;
+    static HashMap<Long, HashMap<Long, Double>> allNodes;
 
     public static void main(String[] args) throws IOException {
-        g = new HashMap<>();
-        f = new HashMap<>();
-        h = new HashMap<>();
-        openList = new LinkedList<>();
-        closedList = new ArrayList<>();
-        allNodes = new ArrayList<>();
+        allNodes = new HashMap<>();
 
         readInput();
         aStarSearch();
     }
 
     public static void readInput() throws FileNotFoundException, NumberFormatException {
-
-        Scanner s = new Scanner(new File("edges.txt"));
-
-
         ArrayList<Long> tempSourceNodes = new ArrayList<>();
         ArrayList<Long> tempDestinationNodes = new ArrayList<>();
         ArrayList<Double> tempDistances = new ArrayList<>();
-        //todo find better way to check if node source node already exists
-        ArrayList<Long> anotherSourceNodeList = new ArrayList<>();
 
+        Scanner s = new Scanner(new File("edges1000.txt"));
         while (s.hasNext()) {
             tempSourceNodes.add(Long.parseLong(s.next()));
             tempDestinationNodes.add(Long.parseLong(s.next()));
             tempDistances.add(Double.parseDouble(s.next()));
         }
 
-        //todo: find more convenient way to check if node source node already exists
-        for (int i = 0; i < tempSourceNodes.size(); i ++){
-            if (tempSourceNodes.contains(tempSourceNodes)) {
-                tempSourceNodes.add(Long.parseLong(sourceNode));
-                System.out.println("yay");
+        for (int i = 0; i < tempSourceNodes.size(); i++) {
+            if (allNodes.containsKey(tempSourceNodes.get(i))) { //if the source node already exists in allNodes
+                HashMap<Long, Double> childNodes = allNodes.get(tempSourceNodes.get(i)); //get the Hashmap of the existing sourceNode
+                childNodes.put(tempDestinationNodes.get(i), tempDistances.get(i)); //put an entry to the hashmap of the existing source node with destination node as key and distance as value
+            } else {
+
+                HashMap<Long, Double> childNodes = new HashMap<>();
+                childNodes.put(tempDestinationNodes.get(i), tempDistances.get(i)); //put an entry to the hashmap of the sourcenode
+                allNodes.put(tempSourceNodes.get(i), childNodes); //
             }
-            allNodes.add(new Node(sourceNode, destNode, distance));
         }
 
-        System.out.println(allNodes);
+        System.out.println("s=");
+        System.out.println("s=" + tempSourceNodes.size());
+        System.out.println("d=" + tempDestinationNodes.size());
+        System.out.println("D=" + tempDistances.size());
+        //for(Map.Entry<Long, HashMap<Long, Double>> entry : allNodes.entrySet()){
+        //    System.out.println(entry);
+        //}
     }
 
     public static void aStarSearch() {
-        openList.add(startingNode);
-        g.put(startingNode, 0.0);
+        //g = new HashMap<>();
+        //f = new HashMap<>();
+        // h = new HashMap<>();
+        openList = new LinkedList<>();
+        closedList = new ArrayList<>();
+
+        openList.add(new Node(startingNode, 0, , allNodes.get(startingNode).keySet())); //insert starting node to queue. destination node is not relevant yet
+
         while (!openList.isEmpty()) {
+            Node currentNode = openList.poll();
+            closedList.add(currentNode);
+            if (currentNode.getNodeId() == goalNode) {
+                System.out.println("we found it yay");
+                return;
+            }
 
-        }
-    }
 
-    public static double findLowest(ArrayList<Double> arrayList) {
-        double lowestCost = 999999;
-        for (int i = 0; i < arrayList.size(); i++) {
-            if (arrayList.get(i) < lowestCost) {
-                lowestCost = arrayList.get(i);
+            for (long childNodeId : currentNode.getChildNodesIds()) {
+
+                double childTotalDistance = currentNode.getCurrentCost() + allNodes.get(currentNode.getNodeId()).get(childNodeId) + heuristic; //current distance + child distance + heuristic distance
+
+                Node childNode = new Node(childNodeId, childTotalDistance);
+
+
+                if (isInOpenList(childNode.getNodeId()) && isBetterInOpenList(childTotalDistance)){
+                    break;
+                }
+                if (isInClosedList(childNode.getNodeId()) && isBetterInClosedList(childTotalDistance)){
+                    break;
+                }
+
+                removeFromOpenList(childNode.getNodeId());
+                removeFromClosedList(childNode.getNodeId());
+
+                openList.add(childNode)
+
+                //if n' is on the OPEN list and the existing one is as good or better
+                // then discard n' and continue
+                // if n' is on the CLOSED list and the existing one is as good or better
+                // then discard n' and continue
+                // Remove occurrences of n' from OPEN and CLOSED
+                // Add n' to the OPEN list
             }
         }
-        return lowestCost;
+
     }
+
+    public static boolean isInOpenList(long nodeId){
+        for(Node nodeInList : openList){
+            long idInList = nodeInList.getNodeId();
+            if(nodeId == idInList) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+
+    public static boolean isBetterInOpenList(double childTotalDistance){
+        for(Node nodeInList : openList){
+            if (nodeInList.getCurrentCost() <= childTotalDistance){
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public static boolean isInClosedList(long nodeId){
+        for(Node nodeInList : closedList){
+            long idInList = nodeInList.getNodeId();
+            if(nodeId == idInList) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public static boolean isBetterInClosedList(double childTotalDistance){
+        for(Node nodeInList : closedList){
+            if (nodeInList.getCurrentCost() <= childTotalDistance){
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public static void  removeFromOpenList(long nodeId){
+        for(Node nodeInList : openList){
+            if (nodeInList.getNodeId() == nodeId ){
+                openList.remove(nodeInList);
+            }
+        }
+    }
+
+    public static void  removeFromClosedList(long nodeId){
+        for(Node nodeInList : closedList){
+            if (nodeInList.getNodeId() == nodeId ){
+                closedList.remove(nodeInList);
+            }
+        }
+    }
+
 }
 
 class Node {
     private long nodeId;
-    private ArrayList<Long> nodeChildIds;
-    private ArrayList<Double> nodeChildDistances;
+    private double currentCost;
+    private Set<Long> childNodesIds;
 
-    Node(long nodeId, long nodeChildId, double nodeChildDistance) {
-        this.nodeChildIds = new ArrayList<Long>();
-        this.nodeChildDistances = new ArrayList<Double>();
-        this.nodeId = nodeId;
-        this.nodeChildIds.add(nodeChildId);
-        this.nodeChildDistances.add(nodeChildDistance);
+    public Node(long sourceNodeId, double currentCost) {
+        this.nodeId = sourceNodeId;
+        this.currentCost = currentCost;
     }
 
-    public void addChildNode(long childNodeID, double distance) {
-        nodeChildIds.add(childNodeID);
-        nodeChildDistances.add(distance);
+    public Node(long sourceNodeId, long currentCost,  Set<Long> childNodesIds) {
+        this.nodeId = sourceNodeId;
+        this.currentCost = currentCost;
+        this.childNodesIds = childNodesIds;
     }
 
     public long getNodeId() {
         return nodeId;
     }
 
-    public void setNodeId(long nodeId) {
-        this.nodeId = nodeId;
+    public void setNodeId(long sourceNodeId) {
+        this.nodeId = sourceNodeId;
     }
 
-    public ArrayList<Long> getNodeChildIds() {
-        return nodeChildIds;
+    public double getCurrentCost() {
+        return currentCost;
     }
 
-    public void setNodeChildIds(ArrayList<Long> nodeChildIds) {
-        this.nodeChildIds = nodeChildIds;
+    public void setCurrentCost(double currentCost) {
+        this.currentCost = currentCost;
     }
 
-    public ArrayList<Double> getNodeChildDistances() {
-        return nodeChildDistances;
+    public Set<Long> getChildNodesIds() {
+        return childNodesIds;
     }
 
-    public void setNodeChildDistances(ArrayList<Double> nodeChildDistances) {
-        this.nodeChildDistances = nodeChildDistances;
+    public void setChildNodesIds(Set<Long> childNodesIds) {
+        this.childNodesIds = childNodesIds;
     }
+
 }
